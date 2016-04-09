a361e2bbc032d188446f74cdafa476e3caa66ce0
Henrique <henriquefc@hotmail.com.br>
2016-04-09 14:07:58 -0300

    Inclusão do Dialog ajuda

--- src/cipriano/view/EditorController.java
+++ src/cipriano/view/EditorController.java
@@ -42,6 +42,25 @@ public class EditorController {
 	}
 
 	@FXML
+	public void ajuda(){
+		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		alert.setTitle("Manual do jNormaMachine");
+		alert.setHeaderText("O programa jNormaMachine é uma ferramenta interativa que demonstra o funcionamento da Máquina de Norma.");
+		alert.setContentText("Syntaxe: \"<Número do rótulo>: <Operação> <Registrador> <Condicional verdadeira> <Condicional falsa>\"\n\n" +
+				"<Número do rótulo>: Qualquer número inteiro não negativo. Representa a identificação do rótulo.\n" +
+				"<Operação>: Operação a ser executa pelo rótulo. Por definição, existem três operações possíveis sobre o valor do registrador. Incrementar um(ADD), decrementar um(SUB) e verificar se é igual a 0 (ZER).\n" +
+				"<Registrador>: Qualquer palavra formada por a-z. Representa o variável que armazenará o valor.\n" +
+				"<Condicional verdadeira>: Indica o próximo rótulo a ser executado caso a operação retorne verdadeiro\n" +
+				"<Condicional falsa>: Valor obrigatório apenas quando a Operação for ZER. Indica o próximo rótulo a ser executado caso a operação retorne falso.\n\n" +
+				"Exemplos: \n" +
+				"\"1: ADD A 2\"\n" +
+				"\"2: ADD B 3 3\"\n" +
+				"\"3: ZER A 2 2\"\n");
+
+		alert.showAndWait();
+	}
+
+	@FXML
 	public void compileAndRun(){
 		try{
 			AnalisadorSemantico.analisa(textArea.getParagraphs());
